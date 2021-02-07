d599987b1039408a47c69461bc46f9279cde2bd4
Muhammed Hanifi Alma <hanifi.alma2014.mha@gmail.com>
2021-02-07 16:49:36 +0300

    google-clone

---
+++ src/keys.js
@@ -0,0 +1,4 @@
+export const API_KEY = "Put API_KEY here ";
+{/*"https://developers.google.com/custom-search/v1/overview", create API_KEY*/}
+
+export default API_KEY;
---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+import Search from "../Search";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+        </div>
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar src="https://avatars.githubusercontent.com/u/76661007?s=460&u=1f4e2928bc46a8ab1e76c5709461dfa02e67336e&v=4"/>
+        </div>
+      </div>
+
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt=""
+        />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
